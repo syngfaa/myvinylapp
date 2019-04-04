3e1675e2cb31ca8b21da789529b65cd49a449ef8
syngfaa <syngfaa@gmail.com>
2019-04-04 13:48:25 +0200

    First setup of my vinyl front end using Angular
    Currently working on tour of heroes course. Translating it to my vinyl purpose. generated a records component and a record detail component. Up next: generate and develop a 'record service'

---
+++ src/app/mock-records.ts
@@ -0,0 +1,14 @@
+import { Record } from './record';
+
+export const RECORDS: Record[] = [
+  { id: 11, title: 'Geography', artist: 'Tom Misch' },
+  { id: 12, title: 'Bloody Tourists', artist: '10cc' },
+  { id: 13, title: 'Breakin Away', artist: 'Al Jarreau' },
+  { id: 14, title: 'Voel de Vibe', artist: 'Brainpower' },
+  { id: 15, title: 'Risque', artist: 'Chic' },
+  { id: 16, title: 'Rebels Are We', artist: 'Chic' },
+  { id: 17, title: 'Erotic Moods', artist: 'George Benson' },
+  { id: 18, title: 'While The City Sleeps...', artist: 'George Benson' },
+  { id: 19, title: 'Cant Slow Down', artist: '	Lionel Richie' },
+  { id: 20, title: 'Your Good Thing', artist: 'Lou Rawls' }
+];
