71c7e5e47188da0c76567a3f2589a2a8d875f7f2
syngfaa <syngfaa@gmail.com>
2019-04-06 17:21:27 +0200

    typo fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { RecordDetailComponent }  from './record-detail/record-detail.component';
-import { RecordsComponent } from './records/Records.component';
+import { RecordsComponent } from './records/records.component';
 
 
 const routes: Routes = [
