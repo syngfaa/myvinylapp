6f93f611429bc1066cccfa54ba48810b5442031b
syngfaa <syngfaa@gmail.com>
2019-04-06 13:03:53 +0200

    Add dashboard link
    Add dashboard link
    Replace ./ (home) link with dashboard link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecordsComponent } from './records/Records.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'records', component: RecordsComponent }
 ];
 
